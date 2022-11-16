1c8963d0cf5d67dab38c4d95c09b6e6103d36452
starboy007-debug <65112799+starboy007-debug@users.noreply.github.com>
2022-11-16 12:41:42 +0530

    BugFix 0.1

--- Challenge/Assets/script/SceneLoad.cs
+++ Challenge/Assets/script/SceneLoad.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class SceneLoad : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         StartCoroutine(Loadlevel());
     }
